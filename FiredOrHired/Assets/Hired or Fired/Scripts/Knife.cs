1a9b46469fac74aae7ee5ba48de724b021764271
Eduuuuuuuuuuuu <eduard.garcia.tobajas@estudiantat.upc.edu>
2025-05-28 13:37:37 +0200

    Simplificación cuchillo

--- FiredOrHired/Assets/Hired or Fired/Scripts/Knife.cs
+++ FiredOrHired/Assets/Hired or Fired/Scripts/Knife.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Knife : MonoBehaviour
 {
-    public Collider bladeCollider;
     public Rigidbody knifeRigidbody;
     private bool isStuck = false;
 
@@ -14,22 +13,8 @@ public class Knife : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (isStuck) return;
-
-        ContactPoint contact = collision.GetContact(0);
-
-        if (contact.thisCollider != bladeCollider) return;
         if (!collision.gameObject.CompareTag("Target")) return;
 
-        isStuck = true;
         knifeRigidbody.isKinematic = true;
-        knifeRigidbody.velocity = Vector3.zero;
-        knifeRigidbody.angularVelocity = Vector3.zero;
-
-        Quaternion currentRot = transform.rotation;
-        Vector3 localOffset = transform.InverseTransformPoint(contact.point);
-        transform.rotation = currentRot;
-        transform.position = contact.point - (transform.rotation * localOffset);
-        transform.SetParent(collision.transform);
     }
 }
