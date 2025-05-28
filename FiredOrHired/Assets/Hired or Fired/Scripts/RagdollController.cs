32a1200bb3fe7be22d32f1f3862934b089df3327
eduardobr33 <100405421+eduardobr33@users.noreply.github.com>
2025-05-28 12:52:52 +0200

    Ragdoll controller fixed

--- FiredOrHired/Assets/Hired or Fired/Scripts/RagdollController.cs
+++ FiredOrHired/Assets/Hired or Fired/Scripts/RagdollController.cs
@@ -3,50 +3,27 @@ using UnityEngine;
 public class RagdollController : MonoBehaviour
 {
     public Animator animator;
-
     private Rigidbody[] ragdollBodies;
-    private Collider[] ragdollColliders;
-
-    [Header("Head Only")]
-    public Rigidbody headRigidbody;
-    public Collider headCollider;
 
     void Awake()
     {
         ragdollBodies = GetComponentsInChildren<Rigidbody>();
-        ragdollColliders = GetComponentsInChildren<Collider>();
 
-        SetFullRagdollState(false);   // Desactiva todo el cuerpo
-        ActivateHeadOnly();           // Solo deja activa la cabeza
+        SetRagdollState(false);
     }
 
     public void ActivateRagdoll()
     {
-        if (animator != null)
-            animator.enabled = false;
-
-        SetFullRagdollState(true);
+        animator.enabled = false;
+        SetRagdollState(true);
     }
 
-    void SetFullRagdollState(bool state)
+    void SetRagdollState(bool state)
     {
         foreach (var rb in ragdollBodies)
         {
             rb.isKinematic = !state;
+            rb.detectCollisions = state;
         }
-
-        foreach (var col in ragdollColliders)
-        {
-            col.enabled = state;
-        }
-    }
-
-    void ActivateHeadOnly()
-    {
-        if (headRigidbody != null)
-            headRigidbody.isKinematic = false;
-
-        if (headCollider != null)
-            headCollider.enabled = true;
     }
 }
